49364da9faabf80846b90e01dc098878b0739224
Denny Syaputra <syaputradenny911@gmail.com>
2026-02-23 09:45:50 +0700

    feat: add ajax and ajaxNoProcessData functions for improved API handling

--- public/assets/js/utilities.js
+++ public/assets/js/utilities.js
@@ -6,3 +6,49 @@ function indicatorOff(selector){
     $(selector).attr('disabled', false)
     $(selector).removeAttr('data-kt-indicator')
 }
+function ajax(type, url, data, onSuccess, beforeSend = null, onFailure = null) {
+    $.ajax({
+        url: url,
+        dataType: 'json',
+        type: type,
+        data: data,
+        beforeSend: beforeSend == null ? showSwalLoader() : beforeSend(),
+        success: (res) => {
+            //refresh csrf
+            csrf_token = res.csrf_token;
+            $("input[name=_token]").val(csrf_token)
+
+            onSuccess(res)
+        }
+    }).fail(() => {
+        if (onFailure == null) {
+            swalFailed()
+        } else {
+            onFailure()
+        }
+    })
+}
+function ajaxNoProcessData(type, url, data, onSuccess, beforeSend = null, onFailure = null) {
+    $.ajax({
+        url: url,
+        dataType: 'json',
+        type: type,
+        data: data,
+        processData: false, // Tell jQuery not to process the data
+        contentType: false, // Tell jQuery not to set contentType
+        beforeSend: beforeSend == null ? showSwalLoader() : beforeSend(),
+        success: (res) => {
+            //refresh csrf
+            csrf_token = res.csrf_token;
+            $("input[name=_token]").val(csrf_token)
+
+            onSuccess(res)
+        }
+    }).fail(() => {
+        if (onFailure == null) {
+            swalFailed()
+        } else {
+            onFailure()
+        }
+    })
+}
